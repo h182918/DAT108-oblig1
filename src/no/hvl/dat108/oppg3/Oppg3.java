7ff090ac9c8608ce941e12125bdf3a320dfaa462
Jocef Iversen <jocefiv@gmail.com>
2018-09-04 11:34:52 +0200

    create classes

---
+++ src/no/hvl/dat108/oppg3/Oppg3.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package no.hvl.dat108.oppg3;
+
+/**
+ * @author jocef
+ *
+ */
+public class Oppg3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ansatt a = new Ansatt("Per", "Askeladden", 'M', "Salg", 500000);
+		System.out.println(a);
+		
+		
+
+	}
+
+}
