58270224d820c01ef0316aed1dc08d10020e4a17
Torkel <182918@stud.hvl.no>
2018-09-04 10:21:22 +0200

    init

---
+++ src/no/hvl/dat108/Buffer.java
@@ -0,0 +1,66 @@
+package no.hvl.dat108;
+
+import java.util.LinkedList;
+
+/**
+ * A shared buffer for consumers and producers. The item type is int. The buffer
+ * is implemented by using a double ended queue.
+ *
+ * @author Atle Geitung
+ * @version 17.05.2018 C++ version
+ * @version 30.08.2018 Java version
+ */
+public class Buffer {
+
+    private final static int SIZE = 10;
+    private LinkedList<Integer> buffer = new LinkedList<Integer>();
+
+    /**
+     * Add a new item to the buffer. If the buffer is full, wait.
+     *
+     * @param item the new item
+     * @throws InterruptedException 
+     */
+    public void add(Integer item) {
+        while (true) {
+            if(buffer.size() < SIZE) {
+            	buffer.add(item);
+            }else {
+            	try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+            return; 
+        	
+        }
+    }
+
+    /**
+     * Remove next available item from the buffer. If the buffer is empty, wait.
+     *
+     * @return next item
+     */
+    public Integer remove() {
+    	Integer back = 0;
+        while (true) {
+            // TODO
+        	if(buffer.size() > 0) {
+            back = buffer.removeFirst();
+        	}else {
+        		try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	}
+            // TODO
+        	return back;
+        }
+        
+    }
+
+}
---
+++ src/no/hvl/dat108/samtid.java
@@ -0,0 +1,15 @@
+package no.hvl.dat108;
+
+public class samtid {
+
+	public static void main(String[] args) {
+		Buffer b = new Buffer();
+		Consumer c1 = new Consumer(b);
+		Producer p1 = new Producer(b);
+		Thread t1 = new Thread(p1);
+		Thread t2 = new Thread(c1);
+		t1.start();
+		t2.start();
+		
+	}
+}
